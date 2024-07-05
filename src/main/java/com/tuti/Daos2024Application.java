456bd3caabfdd735b5136991d0dc7421f3f96e0f
Carolina Martin <83882003+CMLAPAZ@users.noreply.github.com>
2024-07-05 10:05:59 -0300

    Le agregue SecurityScheme

--- src/main/java/com/tuti/Daos2024Application.java
+++ src/main/java/com/tuti/Daos2024Application.java
@@ -7,24 +7,36 @@ import org.springframework.context.annotation.Bean;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 
 @SpringBootApplication
 public class Daos2024Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Daos2024Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Daos2024Application.class, args);
+    }
 
-	  @Bean
-	    public OpenAPI customOpenAPI() {
-	        return new OpenAPI()
-	                .info(new Info()
-	                        .title("API MECA")
-	                        .version("1.0")
-	                        .description("Api del Grupo DAOS = Amor - Ceballos - Espindola - Martin")
-	                        .termsOfService("https://cmlapazonline.com/politica-de-privacidad/")
-	                        .license(new License().name("LICENSE").url("https://tuti.com")));
-	  }
-	        
-	        
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API MECA")
+                        .version("1.0")
+                        .description("Api del Grupo DAOS - Amor - Ceballos - Espindola - Martin")
+                        .termsOfService("https://cmlapazonline.com/politica-de-privacidad/")
+                        .license(new License().name("LICENSE").url("https://tuti.com")));
+    }
+
+    @Bean
+    public SecurityScheme basicAuthScheme() {
+        return new SecurityScheme()
+                .name("basicAuth")
+                .type(Type.HTTP)
+                .scheme("basic")
+                .description("Autenticación Básica");
+    }
 }
+
+
